afe6f1f80444c08272ee744347f080eb5e8c38d5
Sasmit Joshi <sasmitjoshi@student.unsw.edu.au>
2025-06-22 20:45:14 +1000

    Added big chunks of logic for the spending tracker

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spending-Tracker
-Recently, I have been spending too much money. So I wanted something to help me visualise how much money I am spending each month.
+Recently, I have been spending too much money. So I wanted something to help me understand how much money I am spending each month
+as well as how much money is coming into my acount each month.
+
+The UP bank app already has something that calculates all of this for me but I wanted to see if I could make it better.
+
+Also it would look pretty cool as a personal project.
---
+++ tracker.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+
+load_dotenv()
+UP_TOKEN = os.getenv("UP_API_KEY")
+header = {"Authorization": f"Bearer {UP_TOKEN}"}
+
+# Returns all the transactions on my account
+def get_all_transactions():
+    all_transactions = []
+    url = "https://api.up.com.au/api/v1/transactions"
+
+    while url:
+        res = requests.get(url, headers=header)
+        res.raise_for_status()
+        json_data = res.json()
+
+        all_transactions.extend(json_data.get("data", []))
+
+        url = json_data.get("links", {}).get("next")
+
+    return all_transactions
+
+# Cleans the transactions, removing any transactions that are transfers to and from
+# accounts
+def clean_transactions(transactions):
+    cleaned_transactions = []
+    for transaction in transactions:
+        transfer_account = transaction["relationships"].get("transferAccount", {}).get("data")
+        if not transfer_account:
+            cleaned_transactions.append(transaction)
+
+    return cleaned_transactions
+
+# Splits transactions into months of the year
+def split_transactions():
+    print()
+    transactions = get_all_transactions()
+    # need to split transactions into monthly ones
+
+# Running the logic
+if __name__ == "__main__":
+    transactions = get_all_transactions()
+    print(f"Fetched {len(transactions)} transactions")
+    cleaned_transactions = clean_transactions(transactions)
+    for transaction in cleaned_transactions:
+        transaction = transaction["attributes"]
+        desc = transaction["description"]
+        amt = transaction["amount"]["value"]
+        date = transaction["createdAt"]
+        print(f"{date} | {desc} | {amt}")
